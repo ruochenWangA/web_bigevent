52d420c52a2cb70b5d0e6f65501975a3e450fdd0
rcwang <rcwang831@163.com>
2022-02-16 20:51:26 +0800

    完成登录和注册功能的开开发

---
+++ assets/js/baseAPI.js
@@ -0,0 +1,9 @@
+// 每次调用$.get()或$.post()或$.ajax()的时候
+//会先调用ajaxPrefilter这个函数
+//在这个函数中可以拿到我们给ajax提供的配置对象
+$.ajaxPrefilter(function(options) {
+
+    // 再发起真正的ajax请求之前同意拼接请求的根路径
+    options.url = 'http://www.liulongbin.top:3007' + options.url
+    console.log(options.url);
+})
