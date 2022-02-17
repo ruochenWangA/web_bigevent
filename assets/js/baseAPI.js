230dd2267044213229643efcb21d4b9fda6831f9
rcwang <rcwang831@163.com>
2022-02-17 21:25:11 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -5,5 +5,25 @@ $.ajaxPrefilter(function(options) {
 
     // 再发起真正的ajax请求之前同意拼接请求的根路径
     options.url = 'http://www.liulongbin.top:3007' + options.url
-    console.log(options.url);
+        // console.log(options.url);
+
+    //只有url以  /my开头的才是需要权限的
+    if (options.url.indexOf('/my') !== -1) {
+        //统一为有权限的接口设置 headers 请求头
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    //全局统一挂载 complete 回调函数
+    options.complete = function(res) {
+        // 可以使用responseJSON 拿到服务器响应回来的数据
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            //强制情况token
+            localStorage.removeItem('token')
+                // 强制跳转到登陆页面
+            location.href = '/login.html'
+        }
+    }
+
 })
